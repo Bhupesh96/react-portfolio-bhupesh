a543ae220874f51e3f4cc84efecada755b36d590
Bhupesh Sahu <bhupeshsahu9617@gmail.com>
2024-12-02 21:34:37 +0530

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,9 @@ const App = () => {
   return (
     <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
       <div className="fixed top-0 -z-10 h-full w-full">
-      <div class="relative h-full w-full bg-slate-950"><div class="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#3e3e3e,transparent)]"></div></div>      </div>
+        <div class="relative h-full w-full bg-slate-950">
+<div class="relative h-full w-full bg-neutral-900"><div class="absolute inset-0 bg-fuchsia-400 bg-[size:20px_20px] opacity-20 blur-[100px]"></div></div>        </div>{" "}
+      </div>
       <div className="container mx-auto px-8">
         <Navbar />
         <Hero />
@@ -23,6 +25,5 @@ const App = () => {
     </div>
   );
 };
-
 
 export default App;
